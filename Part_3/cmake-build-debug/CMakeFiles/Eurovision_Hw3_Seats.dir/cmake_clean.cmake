f85be85b8f7160f7bb01ee6c0ba235b55d8d2cdf
epreis-pa <58306743+epreis-pa@users.noreply.github.com>
2020-02-25 19:11:44 +0200

    Eurovision Part 3 implementation

---
+++ Part_3/cmake-build-debug/CMakeFiles/Eurovision_Hw3_Seats.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Eurovision_Hw3_Seats.dir/test_part_c.cpp.obj"
+  "Eurovision_Hw3_Seats.exe"
+  "Eurovision_Hw3_Seats.exe.manifest"
+  "Eurovision_Hw3_Seats.pdb"
+  "libEurovision_Hw3_Seats.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Eurovision_Hw3_Seats.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Part_3/seat.h
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::exception;
+
+
+//----------------------------------------------
+class NoPrice : public std::exception
+{
+    virtual char const* what() const noexcept override { return "Not For Sale !"; }
+};
+
+// ---------------------------------------------
+class Seat
+{
+protected:
+    int row_num;
+    int seat_num;
+    int _price;
+    Seat(int row_num, int num, int price) : row_num(row_num) , seat_num(num), _price(price) {};
+public:
+    virtual int price()=0;
+    virtual string location() { return "line: "+std::to_string(row_num)
+                                       +", chair: "+std::to_string(seat_num); };
+    virtual ~Seat() = default;
+};
+
+
+
+// ---------------------------------------------
+class MainHallSeat : public Seat
+{
+protected:
+    const static int mh_price_addition= 100;
+    MainHallSeat(int row_num, int num, int price): Seat(row_num,num,price+mh_price_addition){};
+public:
+    virtual ~MainHallSeat()  = default;
+};
+
+// ---------------------------------------------
+class SpecialSeat : public Seat
+{
+    const static int sps_price_addition = 300;
+protected:
+    SpecialSeat(int row_num, int num, int price) : Seat(row_num,num,price+sps_price_addition) {};
+    virtual ~SpecialSeat() = default;
+};
+
+// ---------------------------------------------
+class GreenRoomSeat: public SpecialSeat
+{
+public:
+    GreenRoomSeat(int row_num, int num): SpecialSeat(row_num,num,0){} ; //sets price to 0 for constructor
+    int price() override{ throw NoPrice(); }
+    string location() override{ return "Green Room-> "+Seat::location(); }
+    ~GreenRoomSeat() override = default;
+};
+
+// ---------------------------------------------
+class GoldenCircleSeat: public SpecialSeat
+{
+    const static int gcs_price_addition = 1000;
+public:
+    GoldenCircleSeat(int row_num, int num, int price): SpecialSeat(row_num,num,price+gcs_price_addition) {};
+    int price() override { return _price; }
+    string location() override { return "Golden Circle-> "+Seat::location(); }
+    ~GoldenCircleSeat() override = default;
+};
+
+// ---------------------------------------------
+class DisablePodiumSeat:public SpecialSeat
+{
+    const static int dps_price = 200;
+public:
+    DisablePodiumSeat(int row_num,int num,int price = dps_price): SpecialSeat(row_num,num,price) {};
+    int price() override { return dps_price; }
+    string location() override { return "Disable Podium-> "+Seat::location(); }
+    ~DisablePodiumSeat() override = default;
+};
+
+// ---------------------------------------------
+class RegularSeat: public MainHallSeat
+{
+protected:
+    char section;
+    RegularSeat(char section, int row_num, int num, int price):
+        MainHallSeat(row_num,num,price), section(section) {};
+    string location() override { return "area: "+string(1,section)+", "+Seat::location(); }
+    virtual ~RegularSeat() = default;
+};
+
+// ---------------------------------------------
+class FrontRegularSeat: public RegularSeat
+{
+    const static int frs_price_addition=500;
+public:
+    FrontRegularSeat(char section, int row, int num, int price) :
+        RegularSeat(section,row,num,price+frs_price_addition) {};
+    int price() override { return _price; }
+    string location() final {
+        return "Front-> "+RegularSeat::location();
+    }
+    ~FrontRegularSeat() override= default;
+};
+
+// ---------------------------------------------
+class MiddleRegularSeat: public RegularSeat
+{
+    const static int mrs_price_addition = 250;
+public:
+    MiddleRegularSeat(char section, int row, int num, int price):
+            RegularSeat(section,row,num,price+mrs_price_addition) {};
+    int price() override { return _price; }
+    string location() override {
+        return "Middle-> "+RegularSeat::location();
+    }
+    ~MiddleRegularSeat() override = default;
+};
+
+// ---------------------------------------------
+class RearRegularSeat: public RegularSeat
+{
+public:
+    RearRegularSeat(char section, int row, int num, int price):
+            RegularSeat(section,row,num,price) {};
+    int price() override { return _price; }
+    string location() override {
+        return "Rear-> "+RegularSeat::location();
+    }
+    ~RearRegularSeat() override = default;
+};
+
+// ---------------------------------------------
